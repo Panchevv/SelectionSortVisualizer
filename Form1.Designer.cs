d75e97f8f306c0a23337ca1bf7ca5acfe17129f2
Panchevv <92977748+Panchevv@users.noreply.github.com>
2022-06-09 13:09:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Selection_Sort_Visualizer
+{
+    partial class SelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNums = new System.Windows.Forms.TextBox();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.labelSwap = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNums
+            // 
+            this.textBoxNums.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNums.Location = new System.Drawing.Point(36, 20);
+            this.textBoxNums.Name = "textBoxNums";
+            this.textBoxNums.Size = new System.Drawing.Size(1095, 27);
+            this.textBoxNums.TabIndex = 1;
+            this.textBoxNums.Text = "29, 72, 98, 13, 87, 66, 52, 51, 36";
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonNext.Location = new System.Drawing.Point(1171, 12);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(176, 42);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // labelSwap
+            // 
+            this.labelSwap.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSwap.Location = new System.Drawing.Point(513, 86);
+            this.labelSwap.Name = "labelSwap";
+            this.labelSwap.Size = new System.Drawing.Size(291, 50);
+            this.labelSwap.TabIndex = 3;
+            this.labelSwap.Text = "Swap";
+            this.labelSwap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSwap.Visible = false;
+            // 
+            // SelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 450);
+            this.Controls.Add(this.labelSwap);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.textBoxNums);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SelectionForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Selection Sort Visualizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNums;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelSwap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selection_Sort_Visualizer
+{
+    public partial class SelectionForm : Form
+    {
+        private int[] array;
+        private int minIndex;
+        private int i;
+        private bool haveMinIndex;
+
+        public SelectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            labelSwap.Visible = false;
+            if(array == null)
+            {
+                i = -1;
+                haveMinIndex = false;
+
+                array = textBoxNums.Text
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => int.Parse(s))
+                    .ToArray();
+                textBoxNums.Visible = false;
+            }
+            Invalidate();
+            Application.DoEvents();
+            Graphics graphics = CreateGraphics();
+            
+            if(i < array.Length)
+            {
+                if (haveMinIndex)
+                {
+                    int temp = array[minIndex];
+                    array[minIndex] = array[i];
+                    array[i] = temp;
+
+                    DrawBoxes(array, graphics, ClientRectangle, Enumerable.Range(0, i + 1).ToArray(), -1, -1);
+                    haveMinIndex = false;
+                }
+                else
+                {
+                    i++;
+                    minIndex = i;
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < array[minIndex])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                    DrawBoxes(array, graphics, ClientRectangle, Enumerable.Range(0, i + 1).ToArray(), minIndex, i);
+                    if(minIndex != i)
+                    {
+                        labelSwap.Visible = true;
+                    }
+                    
+                    if (i == array.Length - 1)
+                    {
+                        return;
+                    }
+                  
+                    haveMinIndex = true;
+                }
+            }
+            else
+            {
+                array = null;
+                textBoxNums.Visible = true;
+            }          
+        }
+
+        protected void DrawBoxes(int[] array, Graphics graphics, Rectangle clientRectangle, int[] blueRange, int min, int swap)
+        {
+            for(int index = 0; index < array.Length; index++)
+            {
+                RectangleF rectangleF = new RectangleF(
+                    clientRectangle.Width / 30 + (clientRectangle.Width / 15) * index + 6 * index,
+                    clientRectangle.Height / 4 + 90,
+                    clientRectangle.Width / 15,
+                    clientRectangle.Height / 7);
+
+                graphics.FillRectangle(Brushes.White, rectangleF);
+
+                if (blueRange.Contains(index))
+                {
+                    graphics.FillRectangle(Brushes.LightBlue, rectangleF);
+                }
+                if (swap == index)
+                {
+                    graphics.FillRectangle(Brushes.Pink, rectangleF);
+                }
+                if (min == index)
+                {
+                    graphics.FillRectangle(Brushes.Green, rectangleF);
+                }
+                
+                graphics.DrawString(
+                    array[index].ToString(),
+                    new Font("Time New Rome", 16),
+                    Brushes.Black,
+                    new RectangleF(
+                    clientRectangle.Width / 30 + (clientRectangle.Width / 15) * index + 6 * index,
+                    clientRectangle.Height / 4 + 90 + 25,
+                    clientRectangle.Width / 15,
+                    clientRectangle.Height / 7 + 25),
+                    StringFormat.GenericDefault);
+
+                using(Pen pen = new Pen(Brushes.Black))
+                {
+                    graphics.DrawRectangle(pen, rectangleF.X, rectangleF.Y, rectangleF.Width, rectangleF.Height);
+                }               
+            }
+        }
+    }
+}
